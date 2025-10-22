5f976d1e7418c7293c5572b3a188d8a2e563f5eb
Shad Matheny <220810537+srmatheny@users.noreply.github.com>
2025-10-21 17:44:36 -0700

    gameboard and eventlisteners for boxes complete

---
+++ main.js
@@ -0,0 +1,76 @@
+
+
+const Gameboard = (() => {
+
+    let gameboard = ["", "", "", "", "", "", "", "", ""]
+
+    const render = () => {
+        let boardHTML = "";
+        gameboard.forEach((square, index) => {
+            boardHTML += `<div class="square" id="square-${index}">${square}</div>`
+        })
+        document.querySelector("#gameboard").innerHTML = boardHTML;
+        const squares = document.querySelectorAll(".square");
+
+        squares.forEach((square) => {
+            square.addEventListener("click", Game.handleClick);
+        });
+
+    };
+
+    return {
+        render, 
+    };
+
+}) ();
+
+
+const createPlayer = (name, mark) => {
+    return {
+        name,
+        mark
+    };
+};
+
+
+const Game = (() => {
+
+    let players = [];
+    let currentPlayerIndex;
+    let gameOver;
+
+    const start = () => {
+        players = [
+            createPlayer(document.querySelector("#player1").value, "X"),
+            createPlayer(document.querySelector("#player2").value, "O")  
+        ];
+        currentPlayerIndex = 0;
+        gameOver = false;
+
+        Gameboard.render();
+    };
+
+    const handleClick = (event) => {
+        let index = parseInt(event.target.id.split("-")[1]);
+        alert(index);
+    };
+
+    return {
+        start, 
+        handleClick
+    };
+
+})();
+
+
+const startButton = document.querySelector("#start-button");
+//console.log(startButton);     
+
+
+
+startButton.addEventListener("click", () => {
+    //alert("Hello World");
+    Game.start();
+
+});
+
